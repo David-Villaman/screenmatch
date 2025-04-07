cfb6c81ff0b3eda419e0a46290b55c42ecca0b5d
David Villaman <davidg.villaman@gmail.com>
2025-04-07 10:27:51 -0700

    uso de constructor en pelicula

--- src/Principal.java
+++ src/Principal.java
@@ -4,25 +4,24 @@ import com.aluracursos.screenmatch.modelos.Episodio;
 import com.aluracursos.screenmatch.modelos.Pelicula;
 import com.aluracursos.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
 
     public static void main(String[] args) {
-        Pelicula pelicula = new Pelicula();
-
-        pelicula.setNombre("Encanto");
+        Pelicula pelicula = new Pelicula("Encanto");
         pelicula.setFechaLanzamiento(2021);
         pelicula.setDuracion(120);
         pelicula.setIncluidoPlan(true);
 
-        pelicula.muestraFicha();
+        //pelicula.muestraFicha();
 
-        pelicula.muestraFicha();
         pelicula.evalua(10);
         pelicula.evalua(10);
         pelicula.evalua(7.8);
-        System.out.println(pelicula.getTotalEvaluaciones());
-        System.out.println(pelicula.calculaMedia());
-        System.out.println("Média de evaluaciones de la película: " +pelicula.calculaMedia());
+       // System.out.println(pelicula.getTotalEvaluaciones());
+        //System.out.println(pelicula.calculaMedia());
+       // System.out.println("Média de evaluaciones de la película: " +pelicula.calculaMedia());
 
 
         Serie casaDragon = new Serie();
@@ -31,10 +30,9 @@ public class Principal {
         casaDragon.setTemporadas(1);
         casaDragon.setMinutosEpisodio(50);
         casaDragon.setEpisodiosTemporada(10);
-        casaDragon.muestraFicha();
+        //casaDragon.muestraFicha();
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.setNombre("Matrix");
+        Pelicula otraPelicula = new Pelicula("Matrix");
         otraPelicula.setFechaLanzamiento(1998);
         otraPelicula.setDuracion(180);
 
@@ -42,10 +40,10 @@ public class Principal {
         calculadora.incluye(pelicula);
         calculadora.incluye(casaDragon);
         calculadora.incluye(otraPelicula);
-        System.out.println("Tiempo necesario para ver todos los titulos: "+calculadora.getTiempoTotal() + " minutos");
+       //` System.out.println("Tiempo necesario para ver todos los titulos: "+calculadora.getTiempoTotal() + " minutos");
 
         FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
-        filtroRecomendacion.filtra(pelicula);
+      //  filtroRecomendacion.filtra(pelicula);
 
 
         Episodio episodio = new Episodio();
@@ -53,7 +51,36 @@ public class Principal {
         episodio.setNombre("La casa Tar");
         episodio.setSerie(casaDragon);
         episodio.setTotalVisualizaciones(300);
-        filtroRecomendacion.filtra(episodio);
+        //filtroRecomendacion.filtra(episodio);
+
+
+        var pelicula2 = new Pelicula("Avatar");
+        pelicula2.setDuracion(150);
+        pelicula2.setFechaLanzamiento(2001);
+
+
+        System.out.println("*********************************");
+
+        ArrayList<Pelicula> peliculas = new ArrayList<>();
+        peliculas.add(pelicula);
+        peliculas.add(otraPelicula);
+        peliculas.add(pelicula2);
+
+        System.out.println("Cantidad de peliculas ingresadas: " +peliculas.size());
+        System.out.println("Nombre de la primer pelicula ingresada:  " +peliculas.get(0).getNombre());
+
+        /*
+         */
+        for (int i = 0; i < peliculas.size(); i++) {
+            Pelicula pelicula1 = peliculas.get(i);
+            System.out.println("Nombre de la pelicula " + (i+1) + ": " +pelicula1.getNombre());
+        }
+
+        System.out.println(peliculas.toString());
+
+        System.out.println("toString de la pelicula: " +peliculas.get(0).toString());
+
+
 
     }
 }
